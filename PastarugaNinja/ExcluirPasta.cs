c236f0af2d8e7dad33d931f38d817c628f3ffc20
Maycon Wisley <maycon.wisley@outlook.com>
2025-11-12 19:56:02 -0300

    ajuste

---
+++ PastarugaNinja/ExcluirPasta.cs
@@ -0,0 +1,21 @@
+﻿namespace PastarugaNinja
+{
+    public static class ExcluirPasta
+    {
+        public static void Excluir(string path)
+        {
+            try
+            {
+                var paths = Directory.GetDirectories(path);
+                foreach (var item in paths)
+                {
+                    Directory.Delete(item);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
